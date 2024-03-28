fe4e59e4d56e41cff46ed6b879cf22aa7b46e18c
Ackimm <ceykerman@gmail.com>
2024-03-28 19:48:29 +0100

    ajout d'un HUD avec timer rudimentaire

--- main.c
+++ main.c
@@ -15,6 +15,91 @@
 #include "enemies.h"
 #include "tirs.h"
 
+/*---------------------test du HUD-----------------------
+---------------------test du HUD-----------------------*/
+
+
+int score = 0;
+int life = 100;
+int time_elapsed = 0;
+time_t start_time;
+
+void startTimer(){
+	start_time = time(NULL);
+
+}
+
+// Fonction de rappel pour mettre à jour le chronomètre
+void updateTimer(int value) {
+    time_elapsed = time(NULL) - start_time ; // Afficher le temps écoulé
+    glutTimerFunc(1000, updateTimer, 0); // Rappeler cette fonction dans 1000 millisecondes (1 seconde)
+}
+
+
+void displayHUD() {
+
+    glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0.0, 1.0, 1.0, 0.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glLoadIdentity();
+
+    glColor4f(0.0, 1.0, 1.0, 0.0); 	// transparence ne marche pas pour le moment, à vérifier
+
+
+	glBegin(GL_QUADS);
+		glVertex2f(0.0, 0.0); // coin sup gauche (puis dans le sens des aiguilles d'une montre)
+		glVertex2f(1.0, 0.0); //coin sup droit
+		glVertex2f(1.0, 0.05); // coin inf droit
+		glVertex2f(0.0, 0.05); // coin inf gauche
+    glEnd();
+
+
+
+	
+
+
+
+ 	// Afficher le score
+    glColor3f(1.0f, 1.0f, 1.0f); // Couleur blanche pour le texte
+    glRasterPos2f(0.2, 0.025); // Position du texte pour le score
+    char score_text[50];
+    sprintf(score_text, "Score: %d", score);
+    for (int i = 0; score_text[i] != '\0'; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, score_text[i]);
+    }
+    
+    // Afficher la vie
+       glRasterPos2f(0.4, 0.025);// Position du texte pour la vie
+    char life_text[100];
+    sprintf(life_text, "Life: %d", life);
+    for (int i = 0; life_text[i] != '\0'; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, life_text[i]);
+    }
+    
+    // Afficher le temps écoulé
+      glRasterPos2f(0.6, 0.025); // Position du texte pour le temps écoulé
+    char time_text[50];
+	glutTimerFunc(1000, updateTimer, 0);
+    sprintf(time_text, "Time: %d", time_elapsed);
+    for (int i = 0; time_text[i] != '\0'; i++) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, time_text[i]);
+    }
+
+
+
+
+
+
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+
+}
 
 
 void initRendering()
@@ -57,9 +142,16 @@ void Display()
     
 	game(&mX, &mY, p, e, t);
 	
+
+
+	
 	//drawWall(mX, mY);
-	glFlush();
+ 	displayHUD(); // associe le HUD au premier ennemi de la listeEn au lieu de le fixer en haut de l'écran
+	glutSwapBuffers();
+	//glFlush();
 }
+
+
 
 int main(int argc, char *argv[])
 {
@@ -77,18 +169,23 @@ int main(int argc, char *argv[])
 	e = initialListEnemies();
 	t = initialListeTirs();
 
+	startTimer();
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
-	glutInitWindowSize((mX)*Square_size, mY*Square_size);
+	glutInitWindowSize((mX-23.2)*Square_size, mY*Square_size); // le -23 ne fait pas vraiment sens pour moi. je l'ai trouvé en chipotant. A creuser
 
 	glutCreateWindow("Ride Or Die! A VvV Story");
 
 	initRendering();
 
 	glutDisplayFunc(Display);
+
+
+
 	glutReshapeFunc(handleResize);
+
 	glutTimerFunc(10, updateEnemies, 1);
 	glutTimerFunc(10, updateNewEnemies, 3);
 	glutTimerFunc(10, updateDeleateEnemies, 4);
